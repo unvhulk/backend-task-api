be042b248aacc63e6d47e6c910292eb2a390d94f
Uday.nv <uday.nv@intechadditive.com>
2025-04-09 16:27:55 +0530

    Fix: Changes in DB.js for railway.app deployment

--- config/db.js
+++ config/db.js
@@ -1,16 +1,9 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    port: process.env.DB_PORT,
-    logging: false,
-  },
-);
+const sequelize = new Sequelize(process.env.MYSQL_URL, {
+  dialect: "mysql",
+  logging: false,
+});
 
 module.exports = sequelize;
